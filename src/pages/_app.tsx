9398b2472601783b723ba63cd7c0a72ea300954c
i.simakin <i.simakin@iz.ru>
2023-02-06 11:35:38 +0300

    mui add

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,33 @@
-import { Provider } from 'react-redux'
-import type { AppProps } from 'next/app'
+import { Provider } from "react-redux"
+import type { AppProps } from "next/app"
+import Head from "next/head"
+import { ThemeProvider } from "@mui/material/styles"
+import CssBaseline from "@mui/material/CssBaseline"
+import { CacheProvider, EmotionCache } from "@emotion/react"
+import theme from "@/template/theme"
+import createEmotionCache from "@/template/createEmotionChache"
 
-import './Global.module.scss'
-import store from '@/redux/store'
+import "./Global.module.scss"
+import store from "@/redux/store"
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache()
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </CacheProvider>
     </Provider>
   )
 }
